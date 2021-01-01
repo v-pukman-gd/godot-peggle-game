e512e4e43b06d8915416b3ea530d5f94821909e5
Victor Pukman <pukman.victor@gmail.com>
2021-01-01 22:06:19 +0300

    use lerp to rotate

--- cannon.gd
+++ cannon.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-const Y_MIN = 100
-
 func _ready():
 	pass
 
@@ -12,22 +10,30 @@ func _input(event):
 func _process(delta):
 	pass
 	
-# Variant 1:
-func follow_cursor(cursor_pos):
-	var x = cursor_pos.x - self.position.x
-	var y = cursor_pos.y - self.position.y
-	if y < 0: y = 0
-	var c = sqrt(x*x + y*y)
-		
-	if c != 0:
-		var angle = -rad2deg(asin(x/c))
-		self.rotation_degrees = angle + 90 
-		#print(angle)
-		
 func _physics_process(delta):
-	if get_global_mouse_position().y >= Y_MIN:
-		follow_cursor(get_global_mouse_position())
+	if get_global_mouse_position().y >= self.position.y:
+		var d = self.global_position - get_global_mouse_position()
+		var offset = -PI
+		var r = d.angle() + offset
+		self.rotation = lerp_angle(self.rotation, r, 0.25)
 	
+# Variant 1:
+#const Y_MIN = 100
+#func follow_cursor(cursor_pos):
+#	var x = cursor_pos.x - self.position.x
+#	var y = cursor_pos.y - self.position.y
+#	if y < 0: y = 0
+#	var c = sqrt(x*x + y*y)
+#
+#	if c != 0:
+#		var angle = -rad2deg(asin(x/c)) + 90
+#		self.rotation_degrees = angle
+#		print(angle)
+#
+#func _physics_process(delta):
+#	if get_global_mouse_position().y >= Y_MIN:
+#		follow_cursor(get_global_mouse_position())
+#
 # Variant2:
 #func _physics_process(delta):
 #	if get_global_mouse_position().y >= Y_MIN:
